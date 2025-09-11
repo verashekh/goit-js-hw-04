a1fe130fc8a20f6e3af1c47711ff06d4a8815ea6
Vera <verashekh@gmail.com>
2025-09-11 12:36:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# goit-js-hw-03
+
+my-js-homework-03
---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+function filterArray(numbers, value) {
+  let Array = [];
+  for (const number of numbers)
+    if (number > value) {
+      Array.push(number);
+    }
+  return Array;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
